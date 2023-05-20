bc58bbf9731df151203ea2df122fce5310de6806
Dark Coder <tapamconsultancy@gmail.com>
2023-05-21 00:16:14 +0300

    Added heroku database variables

--- php/db.php
+++ php/db.php
@@ -1,31 +1,29 @@
 <?php
-// //Database connection.
-// $con = MySQLi_connect(
-//    "localhost", //Server host name.
-//    "root", //Database username.
-//    "", //Database password.
-//    "bakery" //Database name or anything you would like to call it.
-// );
-// //Check connection
-// if (MySQLi_connect_errno()) {
-//    echo "Failed to connect to MySQL: " . MySQLi_connect_error();
-// } else {
-//     echo "Connected ";
-// }
 
-//db constants
 
- $servername = 'localhost';
-    $username = 'root';
-    $password = '';
-    $dbname = "butikkvare";
-    $connection = mysqli_connect($servername, $username, $password, $dbname);
+//  $servername = 'localhost';
+//     $username = 'root';
+//     $password = '';
+//     $dbname = "butikkvare";
+//     $connection = mysqli_connect($servername, $username, $password, $dbname);
       
-    // Check connection
-    if(!$connection){
-        echo('Database connection error : ' .mysql_error());
-    }
+//     // Check connection
+//     if(!$connection){
+//         echo('Database connection error : ' .mysql_error());
+//     }
 
+
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = 'eu-cdbr-west-03.cleardb.net';
+$cleardb_username = 'bc60738b7850a1';
+$cleardb_password = '93349222';
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$connection = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 
 ?>
+
